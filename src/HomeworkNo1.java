1db2d29d10b9dba91a369f24b64f47fc90a59e9b
Zhirayr2000 <zhirayrtovmasyan2000@gmail.com>
2021-09-06 21:30:14 +0400

    hh

---
+++ src/HomeworkNo1.java
@@ -0,0 +1,21 @@
+public class HomeworkNo1 {
+    public static void main(String[] args) {
+        /*System.out.println("Hello");
+        System.out.println("Zhirayr");
+        short sum1=76+1190;
+        short sum2=834+2791;
+        double div1=246/5.0;
+        double div2=1005/28;
+        int exp1=-5+8*6;
+        int exp2=(55+9)%9;
+        double exp3=20+(-3)*5/8.0;
+        int exp4=5+15/3*2-8%3;
+        System.out.println("\u053A\u056B\u0580\u0561\u0575\u0580");
+        char ch1='a';
+        char ch2= 'b';*/
+        System.out.printf("'%-15s' %n","Java");
+        System.out.printf("'%-15s' %n","Cpp");
+        System.out.printf("'%-15s' %n","Python");
+
+    }
+}
